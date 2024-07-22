b7446b48d8b61ebc0f07fb67ec32fef018de5e81
bluefalconhd <hjdombro@icloud.com>
2024-07-22 13:46:15 -0500

    fix/refactor: fix scraping and make operations more efficient.

--- scrape_site.py
+++ scrape_site.py
@@ -1,156 +1,145 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 import re
 import json
 
-scope = 0
 
-# Classes for Texture and Category
 class Texture:
     def __init__(self, path, name):
         self.assetpath = path
         self.name = name
 
     def to_json(self):
-        return {
-             'name': self.name,
-             'path': self.assetpath
-        }
+        return {"name": self.name, "path": self.assetpath}
+
 
 class Category:
-    def __init__(self, title, items):
+    def __init__(self, title):
         self.title = title
-        self.items = items
+        self.items = []
+
+    def add_item(self, texture):
+        self.items.append(texture)
 
     def to_json(self):
-        return {
-    self.title: [texture.to_json() for texture in self.items]
-    }
+        return {self.title: [texture.to_json() for texture in self.items]}
 
-def my_print(t):
-    print(("    " * scope) + t)
 
-# Function to get the previous heading element
-def get_previous_heading_element(element):
-    prev_element = element.find_previous()
-    while prev_element:
-        if prev_element.name in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']:
-            return prev_element
-        prev_element = prev_element.find_previous()
+def get_previous_heading(element):
+    while element:
+        element = element.find_previous()
+        if element and element.name in ["h1", "h2", "h3", "h4", "h5", "h6"]:
+            return element
     return None
 
-# Function to merge categories
-def merge_categories(categories):
+
+def transform_image_url(url):
+    url = "https://minecraft.wiki" + url
+    thumb_index = url.find("/thumb")
+    if thumb_index > -1:
+        first_part = url[:thumb_index]
+        second_part = url[thumb_index + 6 :]
+        last_slash_index = second_part.rfind("/")
+        if last_slash_index > -1:
+            second_part = second_part[last_slash_index + 1 :]
+        url = first_part + "/" + second_part
+    return re.sub(r"\d+px-", "", url)
+
+
+def merge_and_deduplicate_categories(categories):
     merged = {}
+    items_names = set()
+
     for category in categories:
         if category.title not in merged:
-            merged[category.title] = []
-        merged[category.title].extend(category.items)
-    return [Category(title, items) for title, items in merged.items()]
+            merged[category.title] = Category(category.title)
+        for item in category.items:
+            if item.name not in items_names:
+                items_names.add(item.name)
+                merged[category.title].add_item(item)
 
-# Function to transform image URLs
-def transform_image_url(url):
-    # add the main site URL to the start
-    url = "https://minecraft.wiki" + url
+    return list(merged.values())
 
-    
-    # Find the '/thumb' part in the URL and remove it
-    thumb_index = url.find('/thumb')
-    if thumb_index > -1:
-        # Split the URL at the point of '/thumb'
-        first_part = url[:thumb_index]
-        second_part = url[thumb_index + 6:]  # 6 is the length of '/thumb'
 
-        # Find the last slash '/' after '/thumb' which is before the actual file name starts
-        last_slash_index = second_part.rfind('/')
-        if last_slash_index > -1:
-            # Remove everything between '/thumb' and the last slash '/'
-            second_part = second_part[last_slash_index + 1:]
+def my_print(t, scope):
+    print(("    " * scope) + t)
 
-        # Concatenate the first and second part to form the new URL
-        url = first_part + '/' + second_part
 
-    # Remove <any number>px-
-    url = re.sub(r'\d+px-', '', url)
+def scrape_textures(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    category_elements = soup.select(".gallery")
+    categories = []
 
-    return url
+    scope = 0
+    my_print("=== GALLERIES ===", scope)
+    scope += 1
 
-# Request the URL and scrape the content
-url = 'https://minecraft.wiki/w/List_of_block_textures'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-# Find all gallery elements
-category_elements = soup.select(".gallery")
-categories = []
-
-my_print("=== GALLERIES ===")
-
-scope += 1
-
-# Extract information and create Texture and Category objects
-for e in category_elements:
-    if len(e.contents) > 0:
+    for e in category_elements:
         item_elements = e.select(".gallerybox")
-        section_title_element = get_previous_heading_element(e)
-        if section_title_element and section_title_element.select(".mw-headline"):
-            section_title = section_title_element.select(".mw-headline")[0].text.strip()
-        else:
-            section_title = section_title_element.text.strip() if section_title_element else ""
+        section_title_element = get_previous_heading(e)
+        section_title = (
+            section_title_element.select_one(".mw-headline").text.strip()
+            if section_title_element
+            else ""
+        )
 
-        my_print(f"# {section_title}")
-        
-        """
-        
-        .gallerybox
-            .gallerytext
-                <p>
-                    <a>
-                        innerText ; name
-            .thumb
-                <span>
-                    <a>
-                        <img>
-                            src ; texture url
-        
-        """
-
+        my_print(f"# {section_title}", scope)
         scope += 1
-        if item_elements:
-            items = []
-            for elem in item_elements:
-                texture_name = elem.select_one(".gallerytext > p > a")
-                texture_src_url = elem.select_one(".thumb > span > a > img")["src"]
 
-                if texture_name.text != None:
-                    if texture_src_url != None:
-                        texture_url = transform_image_url(texture_src_url)
-                        my_print(f"{texture_name.text.strip()}    ->    ({texture_url})")
-                        items.append(Texture(texture_url, texture_name.text.strip()))
-                    else:
-                        my_print(f"One element in section {section_title} has no src url.")
-                else:
-                    my_print(f"One element in section {section_title} has no name.")
-    
-            if items:
-                category = Category(section_title, items)
-                categories.append(category)
-
+        category = Category(section_title)
+        for elem in item_elements:
+            texture_name_elem = elem.select_one(".gallerytext > p > a")
+            texture_src_elem = elem.select_one(".thumb > span > a > img")
+            if texture_name_elem and texture_src_elem:
+                texture_name = (
+                    texture_name_elem.text.strip()
+                    if texture_name_elem.text
+                    else "Unnamed"
+                )
+                texture_url = transform_image_url(texture_src_elem["src"])
+                my_print(f"{texture_name}    ->    ({texture_url})", scope)
+                category.add_item(Texture(texture_url, texture_name))
+            else:
+                if texture_name_elem is None or texture_name_elem.text is None:
+                    my_print(
+                        f"One element in section {section_title} has no name.", scope
+                    )
+                if texture_src_elem is None:
+                    my_print(
+                        f"One element in section {section_title} has no src url.", scope
+                    )
+        categories.append(category)
         scope -= 1
 
-scope -= 1
+    scope -= 1
 
-my_print("=== MERGING DUPLICATE GALLERIES ===")
+    my_print("=== MERGING DUPLICATE GALLERIES ===", scope)
+    scope += 1
 
-# Merge categories with the same title
-categories = merge_categories(categories)
+    categories = merge_and_deduplicate_categories(categories)
 
-if categories == None:
-    my_print("~~~~~~~~ CATEGORIES == NONE! ~~~~~~~~~")
+    for cat in categories:
+        my_print(f"# {cat.title}", scope)
+        scope += 1
+        for texture in cat.items:
+            my_print(f"{texture.name}    ->    ({texture.assetpath})", scope)
+        scope -= 1
 
-# Convert categories to JSON
-categories_json = {cat.title: [texture.to_json() for texture in cat.items] for cat in categories}
+    scope -= 1
 
-# Write the JSON to a file named 'textures.json'
-with open('textures.json', 'w') as file:
-    json.dump(categories_json, file, indent=2)    
+    return categories
+
+
+def save_categories_to_json(categories, file_name):
+    categories_json = {
+        cat.title: [texture.to_json() for texture in cat.items] for cat in categories
+    }
+    with open(file_name, "w") as file:
+        json.dump(categories_json, file, indent=2)
+
+
+if __name__ == "__main__":
+    url = "https://minecraft.wiki/w/List_of_block_textures"
+    categories = scrape_textures(url)
+    save_categories_to_json(categories, "textures.json")
